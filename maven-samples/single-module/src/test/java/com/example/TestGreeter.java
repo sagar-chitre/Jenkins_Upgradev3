1c1c44ef83f0102455c7f8dccb880f3ae9b6f161
sagar-chitre <74360441+sagar-chitre@users.noreply.github.com>
2020-12-28 19:53:55 +0000

    Update TestGreeter.java
    New test Added

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Test Added by Sagar";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
